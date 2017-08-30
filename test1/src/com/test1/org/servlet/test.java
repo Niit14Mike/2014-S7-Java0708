ca9080a52e4c645e17410f79a24fd10106dfa8e9
QDUmike <360473696@qq.com>
2017-08-30 14:11:19 +0800

    1111

--- test1/src/com/test1/org/servlet/test.java
+++ test1/src/com/test1/org/servlet/test.java
@@ -1,8 +1,14 @@
 package com.test1.org.servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.ServletException;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -26,8 +32,43 @@ public class test extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// TODO Auto-generated method stubection
+		Connection con = null;
+		try{
+			//加载驱动
+			Class.forName("com.mysql.jdbc.Driver");
+			//数据库uri
+			String dburl = "jdbc:mysql://localhost/niit1?"
+					+ "user=root&password=123456789";
+			//获取连接
+			con = DriverManager.getConnection(dburl);
+			
+			Statement stat = con.createStatement();
+			//执行查询语句
+			ResultSet rs = stat.executeQuery("select count(1) name from Student");
+			//获取结果
+			if(rs.next()) {
+				int name = rs.getInt("name");
+				request.setAttribute("name", name);  
+				
+			}
+			
+			
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			if(con != null) {
+				try {
+					con.close();
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	//	response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
 	}
 
 	/**
